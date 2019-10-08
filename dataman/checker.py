de2807dc4d6d263185a338b9874421257e12dfd9
Aaron Bolyard <bolyarda9820@student.faytechcc.edu>
2019-10-08 19:56:07 -0400

    [#5] Add answer checker.

---
+++ dataman/checker.py
@@ -0,0 +1,40 @@
+from input import get_integer_from_user, get_should_continue
+from random import uniform
+from operation import Add, Sub, Multiply, Divide
+from problem import Problem
+
+def generate_problem(rangeMin=1, rangeMax=20):
+	problem_type = round(uniform(1, 4))
+
+	if problem_type == 1:
+		return Problem(Add(), uniform(rangeMin, rangeMax), uniform(rangeMin, rangeMax))
+	elif problem_type == 2:
+		a = uniform(rangeMin, rangeMax)
+		b = uniform(rangeMin, rangeMax)
+		return Problem(Sub(), max(a, b), min(a, b))
+	elif problem_type == 3:
+		return Problem(Add(), uniform(rangeMin, rangeMax), uniform(rangeMin, rangeMax))
+	elif problem_type == 4:
+		a = round(uniform(rangeMin, rangeMax))
+		b = round(uniform(rangeMin, rangeMax))
+		c = a * b
+		return Problem(Divide(), c, min(a, b))
+
+def checker():
+	while True:
+		problem = generate_problem()
+
+		prompt = str.format('{} = ', problem.displayWithoutAnswer())
+		result = get_integer_from_user(prompt)
+
+		if problem.isCorrect(result):
+			print('Correct!')
+		else:
+			print('Incorrect!')
+			print(problem.displayWithAnswer())
+
+		if not get_should_continue():
+			break
+
+if __name__ == '__main__':
+	checker()
---
+++ dataman/input.py
@@ -0,0 +1,21 @@
+def get_integer_from_user(prompt):
+	while True:
+		print(prompt, end='')
+		result = input()
+
+		try:
+			return int(result)
+		except:
+			print('Please enter an integer.')
+
+def get_should_continue():
+	while True:
+		print('Do you want to continue? [Y]es/[N]o: ', end='')
+
+		result = input().lower()
+		if result == 'y':
+			return True
+		elif result == 'n':
+			return False
+		else:
+			print('Please enter "y" for yes or "n" for no.')
